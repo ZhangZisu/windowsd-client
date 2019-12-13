781b7b5f4320fd1adc3e53a22b497d90391abcf6
ZhangZisu <admin@zhangzisu.cn>
2019-12-13 11:08:17 +0800

    Add DNS update hook

--- src/interface/dns.ts
+++ src/interface/dns.ts
@@ -1,8 +1,17 @@
 import { invoke } from '@/router'
 import { cliArgs } from '@/shared/cli'
 import { setDNS } from '@/shared/dns'
+import { bus } from '@/shared/bus'
 
 export async function updateDNS (id: string) {
   const { k, v } = <any> await invoke('dns_upd', { k: cliArgs.hostname, v: cliArgs.device }, { target: id })
   setDNS(k, v)
 }
+
+bus.on('system', (msg) => {
+  if (msg.event === 'online') {
+    const deviceID = <string>msg.deviceID
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    updateDNS(deviceID)
+  }
+})
