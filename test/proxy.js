42a55cb43e49dcfe8e3a0d56b5f83e9688d85ff3
ZhangZisu <admin@zhangzisu.cn>
2019-12-14 20:14:51 +0800

    v1.2.8

--- test/proxy.js
+++ test/proxy.js
@@ -5,7 +5,7 @@ const net = require('net')
 const url = `http://127.0.0.1:5000/${argv.device}/proxy`
 
 net.createServer((socket) => {
-  const conn = request.post(url, { qs: { host: '172.31.11.6', port: '22' } })
+  const conn = request.post(url, { qs: { host: argv.host, port: argv.port } })
   socket.pipe(conn)
   conn.on('response', (res) => {
     if (res.statusCode === 200) {
